38ceda22008b0a2943694e21a706d4a72f03c8a1
Liyanfeng <363476658@qq.com>
2020-01-09 23:45:23 +0800

    modify spawn

--- Assets/Demo/SpawnManager.cs
+++ Assets/Demo/SpawnManager.cs
@@ -1,36 +1,25 @@
-﻿using System.Collections;
-using System.Collections.Generic;
+﻿using System;
 using UnityEngine;
+using UnityEngine.UI;
 
-/// <summary>
-/// demo使用，分配大量的活动的角色
-/// </summary>
-public class SpawnManager : MonoBehaviour {
-
-    #region 字段
-
-    public GameObject spawnPrefab;
-    public int gridWidth;
-    public int gridHeight;
-
-    #endregion
-
-
-    #region 方法
-
-
-    void Start()
+public class SpawnManager : MonoBehaviour 
+{
+    public GameObject[] spawnPrefab;
+    public int count;
+    public Text m_Text;
+    private void Start()
     {
-        for(int i = 0; i < gridWidth; i++)
+        for (var i = 0; i < count; i++)
         {
-            for(int j = 0; j < gridHeight; j++)
+            for (var j = 0; j < spawnPrefab.Length; j++)
             {
-                Instantiate<GameObject>(spawnPrefab, new Vector3(i * 2, 0, j * 2), Quaternion.identity);
+                Instantiate(spawnPrefab[j], new Vector3(i * 2, 0, j * 2), Quaternion.identity);
             }
         }
     }
 
-
-    #endregion
-
+    public void Update()
+    {
+        m_Text.text = (1 / Time.deltaTime).ToString();
+    }
 }
